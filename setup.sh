344423acb0fec33d885536d27c82edbc80fab621
Egor <george.ugolnikov@gmail.com>
2025-05-20 11:00:45 +0300

    Updated README.md

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-cp flask_app.nginx.conf /etc/nginx/sites-available/bells.nginx.conf
+cp flask_app.nginx.conf /etc/nginx/sites-enabled/bells.nginx.conf
 certbot --nginx -d bells.silader.codingporjects.ru
